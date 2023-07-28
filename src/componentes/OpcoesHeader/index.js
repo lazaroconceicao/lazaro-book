b3ab746e4b44adc71f77f2a081dc077e009ace92
lazaroconceicao <joselazaroc@gmail.com>
2023-07-28 09:30:07 -0300

    Alteração dos arquivos em  css pelo styled-components

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,16 +1,32 @@
-import "./estilo.css";
+import styled from "styled-components";
+
+const Opcao = styled.li`
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`;
+
+const Opcoes = styled.ul`
+  display: flex;
+`;
 
 const textoOpoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"];
 
 function OpcoesHeader() {
   return (
-    <ul className="opcoes">
+    <Opcoes>
       {textoOpoes.map((texto) => (
-        <li className="opcao">
+        <Opcao className="opcao">
           <p>{texto}</p>
-        </li>
+        </Opcao>
       ))}
-    </ul>
+    </Opcoes>
   );
 }
 
