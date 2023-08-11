e0463c8536269ff4730c9a8c327e74e6f1a1c03b
lazaroconceicao <joselazaroc@gmail.com>
2023-08-11 15:01:23 -0300

    Props para componente título

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,21 +1,12 @@
 import { livros } from "./dadosUltimosLancamentos";
 import { styled } from "styled-components";
+import { Titulo } from "../Titulo";
 
 const UltimosLancamentosContainer = styled.section`
   background-color: #ebecee;
   padding-bottom: 20px;
   display: flex;
   flex-direction: column;
-`;
-
-const Titulo = styled.h2`
-  width: 100%;
-  padding: 30px 0;
-  background-color: #fff;
-  color: #eb9b00;
-  font-size: 36px;
-  text-align: center;
-  margin: 0;
 `;
 
 const NovosLivrosContainer = styled.div`
@@ -29,7 +20,9 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
   return (
     <UltimosLancamentosContainer>
-      <Titulo>Últimos Lançamentos</Titulo>
+      <Titulo cor="#eb9b00" tamanhoFonte="36px">
+        Últimos Lançamentos
+      </Titulo>
       <NovosLivrosContainer>
         {livros.map((livro) => (
           <img src={livro.src} alt="" />
